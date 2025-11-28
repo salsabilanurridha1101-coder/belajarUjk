ecf30e062e658eaa2db80d7721bff5e01c534cb0
dennisismail608-star <dennisismail608>
2025-11-28 15:07:07 +0700

    db_laundry_database

---
+++ db_laundry_salsa.sql
@@ -0,0 +1,373 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 28 Nov 2025 pada 09.05
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_laundry_salsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customers`
+--
+
+CREATE TABLE `customers` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `address` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `customers`
+--
+
+INSERT INTO `customers` (`id`, `name`, `phone`, `address`, `created_at`, `updated_at`) VALUES
+(1, 'Kairo', '083694521', 'Jl.batu nihil', '2025-11-28 07:45:18', NULL),
+(2, 'omang', '0862145278', 'Jl. inerbang jaya', '2025-11-28 07:46:05', NULL),
+(3, 'Cani', '0896214551', 'Jl.anugrah indah', '2025-11-28 07:46:05', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `levels`
+--
+
+CREATE TABLE `levels` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `levels`
+--
+
+INSERT INTO `levels` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Administrator', '2025-11-28 07:10:23', NULL),
+(2, 'Operator', '2025-11-28 07:10:23', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `level_menus`
+--
+
+CREATE TABLE `level_menus` (
+  `id` int(11) NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `level_menus`
+--
+
+INSERT INTO `level_menus` (`id`, `level_id`, `menu_id`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, '2025-11-28 07:22:13', NULL),
+(2, 1, 2, '2025-11-28 07:39:31', '2025-11-28 07:54:18'),
+(4, 1, 3, '2025-11-28 07:39:31', NULL),
+(5, 1, 4, '2025-11-28 07:39:31', NULL),
+(6, 1, 5, '2025-11-28 07:39:31', NULL),
+(7, 1, 6, '2025-11-28 07:39:31', NULL),
+(8, 1, 7, '2025-11-28 07:39:31', NULL),
+(9, 1, 8, '2025-11-28 07:39:31', NULL),
+(10, 2, 8, '2025-11-28 07:59:23', NULL),
+(11, 2, 7, '2025-11-28 07:59:23', NULL),
+(12, 2, 6, '2025-11-28 07:59:23', NULL),
+(13, 2, 3, '2025-11-28 07:59:23', NULL),
+(14, 2, 2, '2025-11-28 07:59:23', NULL),
+(15, 2, 1, '2025-11-28 07:59:23', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menus`
+--
+
+CREATE TABLE `menus` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `icon` varchar(30) NOT NULL,
+  `link` varchar(30) NOT NULL,
+  `order` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `menus`
+--
+
+INSERT INTO `menus` (`id`, `name`, `icon`, `link`, `order`, `created_at`, `updated_at`) VALUES
+(1, 'dashboard', 'bi bi-app', 'dashboard', 1, '2025-11-28 07:38:34', NULL),
+(2, 'customer', 'bi bi-table', 'customer', 2, '2025-11-28 07:38:34', NULL),
+(3, 'service', 'bi bi-table', 'service', 3, '2025-11-28 07:38:34', NULL),
+(4, 'Level', 'bi bi-table', 'level', 4, '2025-11-28 07:23:37', '2025-11-28 07:59:01'),
+(5, 'user', 'bi bi-people-fill', 'user', 5, '2025-11-28 07:38:34', NULL),
+(6, 'tax', 'bi bi-people', 'tax', 6, '2025-11-28 07:38:34', NULL),
+(7, 'menu', 'bi bi-table', 'menu', 7, '2025-11-28 07:38:34', NULL),
+(8, 'order', 'bi bi-table', 'order', 8, '2025-11-28 07:38:34', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `services`
+--
+
+INSERT INTO `services` (`id`, `name`, `price`, `qty`, `description`, `created_at`, `updated_at`) VALUES
+(1, 'cani', 12000, 1, 'sangat baik', '2025-11-28 07:47:28', NULL),
+(2, 'Aca', 13000, 2, 'hasil bagus', '2025-11-28 07:47:28', NULL),
+(3, 'Vernon', 4000, 2, 'Sangat memuaskan', '2025-11-28 07:47:28', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `taxs`
+--
+
+CREATE TABLE `taxs` (
+  `id` int(11) NOT NULL,
+  `percent` int(11) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `taxs`
+--
+
+INSERT INTO `taxs` (`id`, `percent`, `is_active`, `created_at`, `updated_at`) VALUES
+(1, 10, 1, '2025-11-28 07:47:56', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `trans_orders`
+--
+
+CREATE TABLE `trans_orders` (
+  `id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `order_code` varchar(50) NOT NULL,
+  `order_end_date` date NOT NULL,
+  `order_status` tinyint(1) NOT NULL DEFAULT 0,
+  `order_pay` int(11) NOT NULL,
+  `order_change` int(11) NOT NULL,
+  `order_tax` int(11) NOT NULL,
+  `order_total` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `trans_orders`
+--
+
+INSERT INTO `trans_orders` (`id`, `customer_id`, `order_code`, `order_end_date`, `order_status`, `order_pay`, `order_change`, `order_tax`, `order_total`, `created_at`, `updated_at`) VALUES
+(1, 3, 'ORD-2811251000', '2025-10-31', 1, 0, 0, 1920, 21120, '2025-11-28 08:01:32', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `trans_order_detail`
+--
+
+CREATE TABLE `trans_order_detail` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `qty` decimal(10,1) NOT NULL,
+  `price` int(11) NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `trans_order_detail`
+--
+
+INSERT INTO `trans_order_detail` (`id`, `order_id`, `service_id`, `qty`, `price`, `subtotal`, `created_at`, `updated_at`) VALUES
+(1, 1, 1, 1.6, 12000, 19200, '2025-11-28 08:01:32', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `level_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `level_id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Admin', 'salsa@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2025-11-28 07:05:44', '2025-11-28 07:12:57'),
+(2, 2, 'Operator', 'Operator@gmail.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', '2025-11-28 07:31:42', '2025-11-28 07:42:29');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `levels`
+--
+ALTER TABLE `levels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `level_menus`
+--
+ALTER TABLE `level_menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `menus`
+--
+ALTER TABLE `menus`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `taxs`
+--
+ALTER TABLE `taxs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `trans_orders`
+--
+ALTER TABLE `trans_orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `levels`
+--
+ALTER TABLE `levels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `level_menus`
+--
+ALTER TABLE `level_menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT untuk tabel `menus`
+--
+ALTER TABLE `menus`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `taxs`
+--
+ALTER TABLE `taxs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `trans_orders`
+--
+ALTER TABLE `trans_orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `trans_order_detail`
+--
+ALTER TABLE `trans_order_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
